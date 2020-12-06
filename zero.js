b49ed2708860013f76f5fd9b969038b925be790e
XbabySharkX <muhanwar.us@gmail.com>
2020-12-06 12:53:45 +0700

    Update zero.js

--- zero.js
+++ zero.js
@@ -9,7 +9,7 @@ for (var i = 0; i < navMenu.length; i++) {
     document.getElementById('navmenu').innerHTML += link;
 }
 
-var index = '<div class="contentx"> <h2>Hello Human!</h2> Have fun around in our zone!<br> We are community of Open Source Evangelist and IT Security Researcher, You can call us XbabySharkX.<br> </div><br> Together We <h2>&lt;/learn></h2> Together We <h2>&lt;/can></h2><br> <div class="contentx">When you start to sleep we start studying, when you sleep well, we laugh with our world, enjoy shifting electrons byte by byte.<br><center><h2>&lt;/-Thank You-></h2></center><a href="//www.facebook.com/yaelahwar" target="_blank">&lt;/Facebook></a> <a href="//www.twitter.com/yaelahwar" target="_blank">&lt;/Twitter></a> <a href="//github.com/XbabySharkX" target="_blank">&lt;/Github></a> <a href="//pastebin.com/u/XbabySharkX" target="_blank">&lt;/Pastebin></a></div>';
+var index = '<div class="contentx"> <h2>Hello Human!</h2> Have fun around in our zone!<br> We are community of Open Source Evangelist and IT Security Researcher, You can call us XbabySharkX.<br> </div><br> Together We <h2>&lt;/learn></h2> Together We <h2>&lt;/can></h2><br> <div class="contentx">When you start to sleep we start studying, when you sleep well, we laugh with our world, enjoy shifting electrons byte by byte.<br><center><h2>&lt;/-Thank You-></h2></center><br><br><br><a href="//www.facebook.com/yaelahwar" target="_blank">&lt;/Facebook></a> <a href="//www.twitter.com/yaelahwar" target="_blank">&lt;/Twitter></a> <a href="//github.com/XbabySharkX" target="_blank">&lt;/Github></a> <a href="//pastebin.com/u/XbabySharkX" target="_blank">&lt;/Pastebin></a></div>';
 
 document.getElementById("container").innerHTML = index;
 
